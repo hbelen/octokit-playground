400e83679504649c39097c1be15162eb53f135d8
Hannes Belen <hannesbelen@gmail.com>
2017-11-26 21:57:08 +0100

    Add support for credentials

--- lib/octokit/playground.rb
+++ lib/octokit/playground.rb
@@ -1,7 +1,17 @@
-require "octokit/playground/version"
+require "./playground/version"
+require "Octokit"
+require 'yaml'
 
 module Octokit
   module Playground
-    # Your code goes here...
+  	credentials = YAML.load_file(File.join(__dir__, '../../credentials.yaml'))
+
+    Octokit.configure do |c|
+	  c.login = credentials['login']
+	  c.password = credentials['password']
+	end
+
+	# Fetch the current user
+	puts Octokit.repos({}, query: {type: 'owner', sort: 'asc'})
   end
 end
